65d09ee4329cad15fff4d14eb287893eed3b0b0a
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-05-30 16:18:48 +0200

    aggiunte modifiche varie

---
+++ src/components/CardPage.jsx
@@ -0,0 +1,46 @@
+export default function CardPage (){
+    return(
+        <>
+            <div className="col-3">
+                <div className="card">
+                    <div className="card-header">
+                        <h3>Titolo </h3>
+                    </div>
+                    <div className="card-body">
+                        <p>Descrizione post</p>
+                    </div>
+                </div>
+            </div>
+            <div className="col-3">
+                <div className="card">
+                    <div className="card-header">
+                        <h3>Titolo </h3>
+                    </div>
+                    <div className="card-body">
+                        <p>Descrizione post:</p>
+                    </div>
+                </div>
+            </div>
+            <div className="col-3">
+                <div className="card">
+                    <div className="card-header">
+                        <h3>Titolo </h3>
+                    </div>
+                    <div className="card-body">
+                        <p>Descrizione post:</p>
+                    </div>
+                </div>
+            </div>
+            <div className="col-3">
+                <div className="card">
+                    <div className="card-header">
+                        <h3>Titolo </h3>
+                    </div>
+                    <div className="card-body">
+                        <p>Descrizione post:</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -1,5 +1,15 @@
-export default function ListPage(){
+import CardPage from "../components/CardPage";
+
+export default function ListPage({comics}){
     return (
-        <h1>Lista pagina</h1>
+        <>
+            <h1>Lista di card</h1>
+            <div className="container-list">
+                <div className="row">
+                        <CardPage />
+                </div>
+            </div>
+        </>
+
     )
 }
