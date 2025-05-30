be7d5fbc47e34056193d2b08d98f4f3f5a8053a4
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-05-30 13:04:28 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar";
+import Homepage from "./pages/Homepage";
+
+export default function App(){
+  return (
+    <>
+    <Navbar />
+    <Homepage />
+    </>
+  )
+}
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,7 @@
+export default function Homepage(){
+    return (
+        <main className="container">
+            <h1>My React</h1>
+        </main>        
+    )
+}
