f6f98c2ba87e682f0c63f0f103532f5ce943bde9
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-05-30 14:46:35 +0200

    Aggiunti file Navbar, Layout e Homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./layout/Layout";
 import Homepage from "./pages/Homepage";
+
 
 export default function App(){
   return (
     <>
-    <Navbar />
-    <Homepage />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout/>}>
+           <Route path="/" element={<Homepage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
