d4fbb5f5b1ea64f0c8ad7e29e92add218d267544
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-05-30 15:31:51 +0200

    aggiunta argomenti nelle page

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,6 @@
 export default function Homepage(){
     return (
-        <main className="container">
+        <main className="container-page">
             <h1>Homepage</h1>
         </main>        
     )
--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,5 +1,62 @@
 export default function MyPage(){
     return (
-        <h1>Personale page</h1>
+        <div className="container">
+            <div className="row">
+                <div className="col-3">
+                    <div className="card">
+                        <div className="card-header">
+                            <h4>Dati Personali</h4>
+                        </div>
+                        <div className="card-body">
+                            <h6>Nome:</h6>
+                            <h6>Cognome:</h6>
+                            <h6>Età:</h6>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-3">
+                    <div className="card">
+                        <div className="card-header">
+                            <h4>Hobby</h4>
+                        </div>
+                        <div className="card-body">
+                            <p>Lista hobby:</p>
+                            <ul>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-3">
+                    <div className="card">
+                        <div className="card-header">
+                            <h4>Competenze Lavorative</h4>
+                        </div>
+                        <div className="card-body">
+                            <p>Descrizione:</p>
+                            <p>Competenze acquisite:</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-3">
+                    <div className="card">
+                        <div className="card-header">
+                            <h4>Viaggi</h4>
+                        </div>
+                        <div className="card-body">
+                            <p>Descrizione viaggi:</p>
+                            <p>Mete preferite:</p>
+                            <ul>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
