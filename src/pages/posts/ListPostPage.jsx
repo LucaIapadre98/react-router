51c8ffe71eddf4a0e2410cc0c40a2f7106de8fe8
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-06-03 16:15:12 +0200

    usato map per la lista dei post

--- src/pages/posts/ListPostPage.jsx
+++ src/pages/posts/ListPostPage.jsx
@@ -1,18 +1,36 @@
 import axios from "axios"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export default function ListPostPage(){
+    const [posts, setPosts] = useState([]);
     useEffect(()=>{
         axios.get("http://localhost:3000/posts").then((res)=>{
-            console.log(res);
+           setPosts(res.data.data)
         });
 
     }, []);
     return (
         <div>
             <div className="container">
-                <h1>Lista dei Post:</h1>
+                <h3>Lista dei Post:</h3>
             </div>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                         <th>Title</th>
+                    </tr>
+                </thead> 
+                <tbody>
+                    {posts.map((post)=>(
+                        <tr key={post.id}>
+                            <td>{post.title}</td>
+                            <td>{post.content}</td>
+                            <td>Show</td>
+                        </tr>
+                    ))}
+                </tbody>           
+            </table>
         </div>
     )
 }
